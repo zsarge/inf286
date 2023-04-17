847b34c2ba22fdc652b4ffd9e617046261489c8d
Zack Sargent <zackarywsargent@gmail.com>
2023-04-17 19:29:16 -0400

    draw 1 (one square)

--- conway.js
+++ conway.js
@@ -1,13 +1,41 @@
+class ConwaysGameOfLife {
+  /**
+   * @param { HTMLCanvasElement } canvas
+   */
+  constructor(canvas) {
+    /** @type HTMLCanvasElement */
+    this.canvas = canvas;
+    /** @type CanvasRenderingContext2D */
+    this.context = this.canvas.getContext("2d");
+
+    /** @const {number} */
+    this.SQUARE_SIZE = 50; // size of cell in pixels
+  }
+
+  drawASquare() {
+    let [x, y] = [0, 0];
+    this.context.fillStyle = "white";
+    this.context.fillRect(
+      x * this.SQUARE_SIZE,
+      y * this.SQUARE_SIZE,
+      this.SQUARE_SIZE,
+      this.SQUARE_SIZE
+    );
+
+    this.context.strokeStyle = "white";
+    this.context.strokeRect(
+      x * this.SQUARE_SIZE,
+      y * this.SQUARE_SIZE,
+      this.SQUARE_SIZE,
+      this.SQUARE_SIZE
+    );
+    console.log("square drawn");
+  }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   const canvas = document.getElementById("conway");
-  console.log(canvas);
-  /** @type CanvasRenderingContext2D */
-  const ctx = canvas.getContext("2d");
-  console.log(ctx);
 
-  canvas.width = 500;
-  canvas.height = 500;
-
-  ctx.fillStyle = "black";
-  ctx.fillRect(100, 100, 250, 250);
+  let conway = new ConwaysGameOfLife(canvas);
+  conway.drawASquare();
 });
